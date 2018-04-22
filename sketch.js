d134266f66b4b966f5ef1a60ac7f0751612c5007
Quentin DRUAULT-AUBIN <quentin.druault-aubin@etu.utc.fr>
2018-04-22 19:34:34 +0200

    Squelette

---
+++ sketch.js
@@ -0,0 +1,10 @@
+// Fonction d'initialisation.
+function setup() {
+    // Canvas initial.
+    createCanvas(400, 600);
+}
+
+// Rendu graphique de chaque frame/
+function draw() {
+    background(0);
+}
